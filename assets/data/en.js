7a2b4eb29590cd844f2e90d9f2294cf64857d929
ashish sharma <ashish.sharma@beamstacks.com>
2022-04-04 04:33:07 +0530

    adding base folder repo

--- assets/data/en.js
+++ assets/data/en.js
@@ -6,17 +6,17 @@ var config = {
         "tabs_title" : "Drink what makes you happy!", 
         "tabs" : [
             {
-                "tab_icon" : "/assets/images/svg/basket.svg",
+                "tab_icon" : "/coke/assets/images/svg/basket.svg",
                 "tab_description" : "Promotions <br />& Products",
                 "active": true
             },
             {
-                "tab_icon" : "/assets/images/svg/promotions.svg",
+                "tab_icon" : "/coke/assets/images/svg/promotions.svg",
                 "tab_description" : "Promotions",
                 "active": false
             },
             {
-                "tab_icon" : "/assets/images/svg/recent_order.svg",
+                "tab_icon" : "/coke/assets/images/svg/recent_order.svg",
                 "tab_description" : "Recently Ordered",
                 "active": false
             }
@@ -29,25 +29,25 @@ var config = {
                 "name" : "Kinley 5 case offer",
                 "description" : "7.5 fl oz, 10 pack",
                 "price" : "$14.95",
-                "icon" : "/assets/images/png/product_img.png"
+                "icon" : "/coke/assets/images/png/product_img.png"
             },
             {
                 "name" : "Kinley 5 case offer",
                 "description" : "7.5 fl oz, 10 pack",
                 "price" : "$14.95",
-                "icon" : "/assets/images/png/product_img.png"
+                "icon" : "/coke/assets/images/png/product_img.png"
             },
             {
                 "name" : "Kinley 5 case offer",
                 "description" : "7.5 fl oz, 10 pack",
                 "price" : "$14.95",
-                "icon" : "/assets/images/png/product_img.png"
+                "icon" : "/coke/assets/images/png/product_img.png"
             },
             {
                 "name" : "Kinley 5 case offer",
                 "description" : "7.5 fl oz, 10 pack",
                 "price" : "$14.95",
-                "icon" : "/assets/images/png/product_img.png"
+                "icon" : "/coke/assets/images/png/product_img.png"
             }
         ]
     },
@@ -70,92 +70,92 @@ var config = {
     },
     "products" : [
         {
-            "icon" : "/assets/images/svg/coke_product.svg",
+            "icon" : "/coke/assets/images/svg/coke_product.svg",
             "volume" : "2 Ltr",
             "items": [
                 {
                     "name" : "Kinley 5 case offer",
                     "description" : "7.5 fl oz, 10 pack",
                     "price" : "$14.95",
-                    "icon" : "/assets/images/svg/coke_xl.svg"
+                    "icon" : "/coke/assets/images/svg/coke_xl.svg"
                 },
                 {
                     "name" : "Kinley 5 case offer",
                     "description" : "7.5 fl oz, 10 pack",
                     "price" : "$14.95",
-                    "icon" : "/assets/images/svg/sprite_xl.svg"
+                    "icon" : "/coke/assets/images/svg/sprite_xl.svg"
                 },
                 {
                     "name" : "Kinley 5 case offer",
                     "description" : "7.5 fl oz, 10 pack",
                     "price" : "$14.95",
-                    "icon" : "/assets/images/svg/fanta_xl.svg"
+                    "icon" : "/coke/assets/images/svg/fanta_xl.svg"
                 },
                 {
                     "name" : "Kinley 5 case offer",
                     "description" : "7.5 fl oz, 10 pack",
                     "price" : "$14.95",
-                    "icon" : "/assets/images/svg/coke_zero_xl.svg"
+                    "icon" : "/coke/assets/images/svg/coke_zero_xl.svg"
                 }
             ] 
         },
         {
-            "icon" : "/assets/images/svg/fanta_xl.svg",
+            "icon" : "/coke/assets/images/svg/fanta_xl.svg",
             "volume" : "1 Ltr",
             "items": [
                 {
                     "name" : "Kinley 5 case offer",
                     "description" : "7.5 fl oz, 10 pack",
                     "price" : "$14.95",
-                    "icon" : "/assets/images/svg/fanta_xl.svg"
+                    "icon" : "/coke/assets/images/svg/fanta_xl.svg"
                 },
                 {
                     "name" : "Kinley 5 case offer",
                     "description" : "7.5 fl oz, 10 pack",
                     "price" : "$14.95",
-                    "icon" : "/assets/images/svg/coke_xl.svg"
+                    "icon" : "/coke/assets/images/svg/coke_xl.svg"
                 },
                 {
                     "name" : "Kinley 5 case offer",
                     "description" : "7.5 fl oz, 10 pack",
                     "price" : "$14.95",
-                    "icon" : "/assets/images/svg/sprite_xl.svg"
+                    "icon" : "/coke/assets/images/svg/sprite_xl.svg"
                 },
                 {
                     "name" : "Kinley 5 case offer",
                     "description" : "7.5 fl oz, 10 pack",
                     "price" : "$14.95",
-                    "icon" : "/assets/images/svg/coke_zero_xl.svg"
+                    "icon" : "/coke/assets/images/svg/coke_zero_xl.svg"
                 }
             ] 
         },
         {
-            "icon" : "/assets/images/svg/sprite_product.svg",
+            "icon" : "/coke/assets/images/svg/sprite_product.svg",
             "volume" : "250 Ml",
             "items": [
                 {
                     "name" : "Kinley 5 case offer",
                     "description" : "7.5 fl oz, 10 pack",
                     "price" : "$14.95",
-                    "icon" : "/assets/images/svg/sprite_xl.svg"
+                    "icon" : "/coke/assets/images/svg/sprite_xl.svg"
                 },
                 {
                     "name" : "Kinley 5 case offer",
                     "description" : "7.5 fl oz, 10 pack",
                     "price" : "$14.95",
-                    "icon" : "/assets/images/svg/coke_xl.svg"
+                    "icon" : "/coke/assets/images/svg/coke_xl.svg"
                 },
                 {
                     "name" : "Kinley 5 case offer",
                     "description" : "7.5 fl oz, 10 pack",
                     "price" : "$14.95",
-                    "icon" : "/assets/images/svg/fanta_xl.svg"
+                    "icon" : "/coke/assets/images/svg/fanta_xl.svg"
                 },
                 {
                     "name" : "Kinley 5 case offer",
                     "description" : "7.5 fl oz, 10 pack",
                     "price" : "$14.95",
-                    "icon" : "/assets/images/svg/coke_zero_xl.svg"
+                    "icon" : "/coke/assets/images/svg/coke_zero_xl.svg"
                 }
             ] 
         }
--- assets/js/home.js
+++ assets/js/home.js
@@ -1,7 +1,7 @@
 $(function () {
-    $("#bodyContent").load("/views/body.html");
+    $("#bodyContent").load("/coke/views/body.html");
     var scriptTag = document.createElement('script');
-    scriptTag.src = "/assets/js/custom.js";
+    scriptTag.src = "/coke/assets/js/custom.js";
     scriptTag.type = "text/javascript";
     document.getElementById('bodyContent').append(scriptTag);
 });
