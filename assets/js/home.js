4c57a2d8bdc5c083d76315902544b5e2e060b9eb
ashish sharma <ashish.sharma@beamstacks.com>
2022-04-04 03:53:36 +0530

    make code dynamic

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,7 @@
 $(function () {
     $("#bodyContent").load("/views/body.html");
+    var scriptTag = document.createElement('script');
+    scriptTag.src = "/assets/js/custom.js";
+    scriptTag.type = "text/javascript";
+    document.getElementById('bodyContent').append(scriptTag);
 });
