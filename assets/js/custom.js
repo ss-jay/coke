43565e1085be9e80f5a5b6cf2ce8bef69b5ef54c
ss-jay <>
2022-04-26 13:05:02 +0530

    comment insertSelectedCoupon

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -589,7 +589,7 @@ function updateCheckoutCartData(data, type) {
             "product_data": data,
             "quantity": 1
         }
-        insertSelectedCoupon(config.checkout.discounts[0]);
+        // insertSelectedCoupon(config.checkout.discounts[0]);
         processQ(cartData, data.sku);
         return;
     }
