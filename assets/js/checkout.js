4246a66000119635266c5bb469c8ba2fe5cac859
ashish sharma <ashish.sharma@beamstacks.com>
2022-04-27 14:22:09 +0530

    change loc. of recalc disc

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -229,6 +229,7 @@ function insertDiscountSection(discountAvailable) {
     /* $('.offer__apply').click(function () {
         addDiscount(this);
     }); */
+    recalculateCart();
 }
 
 function insertDistributorAddress() {
@@ -329,7 +330,6 @@ function addDiscount(node) {
 
 function processQ(data, skuid) {
     insertOrderCart(data, skuid);
-    recalculateCart(config.checkout.discounts[1]);
     passDataToBot(data);
 }
 
